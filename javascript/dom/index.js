5bf0ce395bb4bca6f0d62e883d5f4cf01ce6d692
Tanuj <tanujkundlas32@gmail.com>
2023-09-08 14:13:01 +0530

    added all file

---
+++ javascript/dom/index.js
@@ -0,0 +1,37 @@
+
+
+// Dom have 4 pillars!
+// 1. Selection of an Element
+// 2. Changing in HTML
+// 3. Changing in CSS
+// 4. Event listener
+
+
+// selection in javascript 
+
+
+// document.querySelector("#id");
+// document.querySelector("#class");
+// var a = document.querySelector("h1");
+// console.log(a);
+
+// Changing in HTML
+
+// var a = document.querySelector("h1").innerHTML = "lolo";
+// a.innerHTML = "mohit  chutiya!"
+
+// Changing in CSS
+
+// var a = document.querySelector("h1");
+// a.style.color = "pink";
+// a.style.backgroundColor = "violet";
+
+// Event Listener
+
+// var a = document.querySelector("h1");
+
+// a.addEventListener("click",function(){
+//     a.innerHTML = "Mukul bevkoof hai";
+//     a.style.color = "yellow";
+//     a.style.backgroundColor = "black";
+// })
